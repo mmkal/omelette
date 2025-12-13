8e1c39167c4de195dc266f9699373c6e2bd8d896
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-12-13 12:04:14 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 npx tsc
-# put `else` statements on the same line as preceding `}` - just to match prior formatting to reduce diff noise - can remove after coffee->typescript migration is complete
+# put `else` statements on the same line as preceding `}` - just to match prior coffeescript formatting to reduce diff noise - can remove after coffee->typescript migration is complete
 node -e '
 var fs=require("fs");
 const content = fs.readFileSync("src/omelette.js", "utf8");
